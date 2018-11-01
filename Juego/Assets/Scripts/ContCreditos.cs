779450868d7388798857045998acc140641f215b
matu <matias.abarquez@hotmail.com>
2018-11-01 14:21:57 -0300

    +No se agrego script

---
+++ Juego/Assets/Scripts/ContCreditos.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContCreditos : MonoBehaviour {
+    public static int contCreditos = 0;
+
+
+    private void Update()
+    {
+        Debug.Log(contCreditos);
+    }
+}
